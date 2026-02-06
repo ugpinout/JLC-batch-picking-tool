2f0df41a4ccc1f3232c08fc813ea5c03bbb72623
ugpinout <73376078+ugpinout@users.noreply.github.com>
2026-02-06 21:55:27 +0800

    Master

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * 入口文件
+ *
+ * 本文件为默认扩展入口文件，如果你想要配置其它文件作为入口文件，
+ * 请修改 `extension.json` 中的 `entry` 字段；
+ *
+ * 请在此处使用 `export`  导出所有你希望在 `headerMenus` 中引用的方法，
+ * 方法通过方法名与 `headerMenus` 关联。
+ *
+ * 如需了解更多开发细节，请阅读：
+ * https://prodocs.lceda.cn/cn/api/guide/
+ */
+import * as extensionConfig from '../extension.json';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function activate(status?: 'onStartupFinished', arg?: string): void {}
+
+export function about(): void {
+	eda.sys_Dialog.showInformationMessage(
+		eda.sys_I18n.text('EasyEDA extension SDK v', undefined, undefined, extensionConfig.version),
+		eda.sys_I18n.text('About'),
+	);
+}
+export function BPT_SCH(): void {
+	eda.sys_IFrame.openIFrame('/iframe/BPT_SCH.html', 900, 700);
+}
+export function BPT_PCB(): void {
+	eda.sys_IFrame.openIFrame('/iframe/BPT_PCB.html', 900, 700);
+}
